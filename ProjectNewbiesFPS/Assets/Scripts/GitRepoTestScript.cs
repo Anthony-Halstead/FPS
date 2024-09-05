20d6829d7a3324924fedde0ac3983ed4c2785b50
Anthony <anthony.dan.hal@gmail.com>
2024-09-04 20:26:55 -0500

    This is a test script update (ignore)

---
+++ ProjectNewbiesFPS/Assets/Scripts/GitRepoTestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitRepoTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Testing Testing One Two");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
