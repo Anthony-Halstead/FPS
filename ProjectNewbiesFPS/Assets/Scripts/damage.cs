41a3857793582b487f85f7c6b851a75ca7c3e4e7
Keagon King <keagonk@gmail.com>
2024-09-11 12:13:26 -0500

    Player and Enemy fixed

---
+++ ProjectNewbiesFPS/Assets/Scripts/damage.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    enum damageType { moving, stationary, melee }
+    
+    [SerializeField] private damageType type;
+    [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private int damageAmount;
+    [SerializeField] private int speed;
+    [SerializeField] private int destroyTime;
+
+    [SerializeField] private bool shouldTrack;
+
+    private void Start()
+    {
+        if (type == damageType.moving)
+        {
+            if (!shouldTrack)
+            {
+                rb.velocity = transform.forward * speed;
+            }
+            else
+            {
+                rb.velocity = (GameManager.instance.player.transform.position - transform.position).normalized * speed;
+            }
+            
+            Destroy(gameObject, destroyTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger) return;
+        
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null)
+        {
+            dmg.takeDamage(damageAmount, Vector3.zero);
+        }
+
+        if (type == damageType.moving)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
