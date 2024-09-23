6229e936cb7a32af307a636bd23fe66cd244733c
Anthony <anthony.dan.hal@gmail.com>
2024-09-23 17:28:17 -0500

    AI functionality
    Added new AI functionality to include a dodge and sweep attack state. All states are still in development and should not be added yet

--- ProjectNewbiesFPS/Assets/Scripts/AI/States/DodgeState.cs
+++ ProjectNewbiesFPS/Assets/Scripts/AI/States/DodgeState.cs
@@ -5,6 +5,7 @@ public class DodgeState : State
 {
     public State guardState;
     [SerializeField] private float dodgeDistance = 5;
+    [SerializeField] private float dodgeCooldown = 10f;
     public override void EnterState(AIController controller)
     {
         controller.positionRoutine = controller.StartCoroutine(controller.GetRandomClearPositionInRange(dodgeDistance));
@@ -16,11 +17,12 @@ public class DodgeState : State
             controller.Anim.SetTrigger("Dodge");
             controller.PathFound = false;
         }
-
-    
-                controller.TransitionToState(guardState);
-          
-         
+        if (controller.CanTransition)
+        {
+            controller.CanTransition = false;
+            controller.DodgeCooldownTimer = dodgeCooldown;
+            controller.TransitionToState(guardState);
+        }     
     }
     public override void ExitState(AIController controller)
     {
--- ProjectNewbiesFPS/Assets/Scripts/AI/States/GuardState.cs
+++ ProjectNewbiesFPS/Assets/Scripts/AI/States/GuardState.cs
@@ -7,17 +7,29 @@ public class GuardState : State
 {
     public State chaseState;
     public State dodgeState;
+    public State sweepState;
     public override void EnterState(AIController controller)
     {
-     
+        controller.StopRig();
+        controller.Agent.angularSpeed = 0f;
+        controller.target = controller.transform.position;
+        controller.lookTarget = controller.transform.forward;
+        
     }
     public override void UpdateState(AIController controller)
     {
-        if (controller.TargetIsVisible())
+        if (chaseState != null && !controller.IsShooting && controller.IsTakingDamage && controller.TargetIsVisible())
         {
             controller.TransitionToState(chaseState);
         } 
-       // if(dodgeState != null &&)
+        if (dodgeState != null && controller.DodgeCooldownTimer <= 0 && controller.IsTakingDamage)
+        {
+            controller.TransitionToState(dodgeState);
+        }
+        if(sweepState != null && controller.SweepCooldownTimer <= 0 && controller.TargetIsVisible())
+        {
+            controller.TransitionToState(sweepState);
+        }
     }
     public override void ExitState(AIController controller)
     {
